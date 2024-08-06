9ac9757d31d02555e9b4eb5af8e8df9aafc76564
RifkyTKJX-A <rifkyseptian238@gmail.com>
2024-08-05 14:48:10 -1200

    Add files via upload

---
+++ pertemuan 5/nilai.py
@@ -0,0 +1,10 @@
+nilai = 80
+
+if nilai >=90 and nilai <=100:
+    print ("Garde A")
+elif nilai > 80 and nilai < 91: 
+    print ("Garde B")
+elif nilai >= 75 and nilai <= 80:
+    print ("Garde C")
+else:
+    print ("Garde D")
