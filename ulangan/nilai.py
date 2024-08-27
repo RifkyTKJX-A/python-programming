f14bcfa3edd5dd4feddf3538d221d8e1ab5a4704
RifkyTKJX-A <rifkyseptian238@gmail.com>
2024-08-27 09:52:53 +0700

    Add files via upload

---
+++ ulangan/nilai.py
@@ -0,0 +1,26 @@
+import time
+
+a = int(input("masukan jumlah siswa: "))
+
+for i in range (a):
+    nama = (input("masukan nama siswa / siswi: "))
+    tugas1 = float(input("masukan nilai tugas ke 1 anda"))
+    tugas2 = float(input("masukan nilai tugas ke 2 anda"))
+    tugas3 = float(input("masukan nilai tugas ke 3 anda"))
+
+b = 3
+
+all = tugas1 + tugas2 + tugas 3
+
+avv = all // b
+
+if  avv >= 70:
+    print("selamat kamu lulus")
+elif avv >= 50 dan 69:
+    print("anda harus perbaikan")    
+elif avv <50:
+    print("anda tidak lulus")    
+else:
+    print("anda tidak lulus, jangan lupa belajar lagi ya") 
+    print(selamat",nama,"nilai rata rata kamu = ", avv, semangat belajarnya ya")
+      
